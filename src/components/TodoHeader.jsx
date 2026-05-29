9ca1f4080b1747c171368ada5f0e4205ae3256b1
hJun-KR <h0ngjunp.park@gmail.com>
2026-05-29 14:50:27 +0900

    feat: 추가 실시간 시간 표시

--- src/components/TodoHeader.jsx
+++ src/components/TodoHeader.jsx
@@ -1,5 +1,23 @@
+import React from "react";
+import { useState, useEffect } from 'react'
+
 export default function TodoHeader() {
+    const [time, setTime] = useState(new Date());
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTime(new Date());
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, []);
+
     return (
-        <h1 className="todo__title">HyeonDo List</h1>
+        <>
+            <h1 className="todo__title">HyeonDo List</h1>
+            <h2 className="todo__clock">{
+                time.toLocaleTimeString('ko-KR', { hour: '2-digit', minute: '2-digit', second: '2-digit' })
+            }</h2>
+        </>
     )
 }
