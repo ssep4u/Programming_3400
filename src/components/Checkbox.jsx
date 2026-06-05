d3ea878ebf9f69266e08c6e996cd2fd788645cfc
Lee Hyo-eun <hi285283@gmail.com>
2026-06-05 13:36:27 +0900

    3411 이효은 1-1	날짜 표시?	★	Date, 컴포넌트 렌더링

--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -1,9 +1,12 @@
 export default function Checkbox(props) {
-    const { children, id, ...rest } = props
+    const { children, id, createdDate, ...rest } = props
     return (
         <>
             <input type="checkbox" id={`chk-${id}`} className="todo__check" {...rest} />
-            <label htmlFor={`chk-${id}`} className="todo__label">{children}</label>
+            <label htmlFor={`chk-${id}`} className="todo__label">
+                {children}
+                {createdDate && <span className="todo__date">{createdDate}</span>}
+            </label>
         </>
     )
 }
